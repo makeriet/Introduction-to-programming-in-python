fdf3f91b15e522778d0fc62fa03b11f75fb3a49f
OsloMet Makerspace <31694254+makeriet@users.noreply.github.com>
2019-05-21 04:46:50 +0200

    Update C_functions.py

--- C_functions.py
+++ C_functions.py
@@ -3,68 +3,22 @@
 #def functionName(parameters):
     # statement(s)
 
-"""2. Types of functions"""
+"""
+2. Types of functions
+"""
 
-#(a)Non-value returning
-
-def compareNumber(a, b):
-    if (a>b):
-        print("First number is greater than second number")
-    elif(a<b):
-        print("First number is less than second number")
-    else:
-        print("First num is equals to second number")
+    #(a)Non-value returning
 
 
-#(b)Value returning
+    #(b)Value returning
+    
 
-def findAverage(newList):
-    numberSum = 0
-    for eachNumber in newList:
-        numberSum = numberSum + eachNumber
-        avg_number = numberSum/len(newList)
-
-    return avg_number
+"""
+3. Calling functions, 
+    with/without Arguments and parameters
+"""
 
 
+#Example C: Write a program to find average of numbers in list
 
-#3. Calling functions
-
-firstNum = int(input("Enter first Number: "))
-secondNum = int(input("Enter second Number: "))
-compareNumber(firstNum, secondNum)
-
-
-#4. Arguments and parameters
-
-numberList = [34, 66, 72, 88, 18]
-newAverage = findAverage(numberList)
-print("The average of list is:",newAverage)
-
-#Example C: Comparing numbers
-    #Write a program to find average of numbers in list
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        # We are using value returning function
