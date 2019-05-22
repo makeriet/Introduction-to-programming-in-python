b778d2ead4ad4cb486746a8cbae6211399dc14b3
OsloMet Makerspace <31694254+makeriet@users.noreply.github.com>
2019-05-22 16:47:47 +0200

    Update C_functions.py

--- C_functions.py
+++ C_functions.py
@@ -7,42 +7,22 @@
 
 #(a)Non-value returning
 
-def compareNumber(a, b):
-    if (a>b):
-        print("First number is greater than second number")
-    elif(a<b):
-        print("First number is less than second number")
-    else:
-        print("First num is equals to second number")
 
 
 #(b)Value returning
 
-def findAverage(newList):
-    numberSum = 0
-    for eachNumber in newList:
-        numberSum = numberSum + eachNumber
-        avg_number = numberSum/len(newList)
 
-    return avg_number
 
 #3. Calling functions
 
-firstNum = int(input("Enter first Number: "))
-secondNum = int(input("Enter second Number: "))
-compareNumber(firstNum, secondNum)# calling compareNumber function and sending firstNum and secondNum as argument
-#compareNumber function is non value returing function
+
 
 #4. Arguments and parameters
 
-numberList = [34, 66, 72, 88, 18]
-newAverage = findAverage(numberList)# calling findAverage function and sending numberList as argument
-#findAverage function is value returning function
 
-print("The average of list is:",newAverage)
 
-#Example C: Comparing numbers
-    #Write a program to find average of numbers in list
+
+#Example C: Write a program to find average of numbers in list and comparing given two numbers
     #===>check file named Example_C.py
     
     
