f2204568c8e71dfbe6bed2628de247a472994f59
OsloMet Makerspace <31694254+makeriet@users.noreply.github.com>
2019-05-22 16:34:17 +0200

    Update B_control_flow.py

--- B_control_flow.py
+++ B_control_flow.py
@@ -4,25 +4,38 @@
 #3. Selection control flow
     #if, else, Else If (elif)
 
-        """
-        == equals to
-        != not equals to
-        < less than
-        > greater than
-        >= less or equals to
-        <= greater or equals to
-        """
+x = 12
+y = 6
+if (x == y):
+    print("x is equal to y")
+elif (x>y):
+    print("x is greater than y")
+else:
+    print("x is  equal to y")
+
+# == equals to
+# != not equals to
+# < less than
+# > greater than
+# >= less or equals to
+# <= greater or equals to
 
 #4. Iterative control flow
-    ###while loop###
+    #while loop
+
+number = 0
+while(number<10):
+    print(number)
+    number = number + 1
 
     ### for loop ###
+expenses = [1888, 3000, 7000, 6458, 1290]
+for eachMonthExpense in expenses:
+    print(expenses)
 
 # Example B: Guess the number game
 
-
-
-
+  #===>check file named Example_B.py
 
 
 
