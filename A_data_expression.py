13515ca3f3e21d637664df3b691b01dbc5d1c0e0
OsloMet Makerspace <31694254+makeriet@users.noreply.github.com>
2019-05-22 16:14:19 +0200

    Update A_data_expression.py

--- A_data_expression.py
+++ A_data_expression.py
@@ -1,7 +1,7 @@
 #1. Commenting
 
 #2. First program
-print("Hello World!")
+#print("Hello World!")
 
 
 #3. Variables and Data Types
@@ -10,6 +10,15 @@ Integer, Float, String
 List, Boolean
 """
 
+x = 10.89 #variable x has float data
+
+name = "Oslo Met" #variable name has string data
+
+studentInfo = ["Tom", 25, 1999.89] # variable studentInfo is a list containing ["string", integer, float] data
+
+valid = True #variable valid has boolean data
+
+
 #4. Arithmetic Operation
 """
 + = addition, - = subtraction, * = multiplication, / = division
@@ -17,14 +26,25 @@ List, Boolean
 ** = exponentiation/power 
 """
 
+num1= 65
+num2 = 6
+
+print(num1** num2) #try using different arithmetic operators
+
+
+
 #5. Converting data types
 """
 Int to String, Float to String,
 String to Int
 """
 
+
 #Example A: Ask user to input length and breadth and find
 # area and perimeter of rectangle.
+
+	#===> check file named Example_A.py
+
 
 
 
